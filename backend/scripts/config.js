2e3870c26d5438a7d0c35fb9cc15f5122fb027dd
Quicklink <33082510+QuickLinkNet@users.noreply.github.com>
2025-02-16 20:24:50 +0100

    Rewmoved old Backend

--- backend/scripts/config.js
+++
@@ -1,6 +0,0 @@
-// Server Konfiguration
-module.exports = {
-    SERVER: 'u2915078@h2915078.stratoserver.net',
-    TARGET_PATH: '/kunden/pages/b1/f3/d0015521/home/htdocs/x/backend',
-    PORT: 3001
-};
